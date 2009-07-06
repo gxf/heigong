e3a4a051ef840aae20645b57ff52da4c7f13ccb2
Donald <donald@donald-e1000h.(none)>
2009-07-06 23:46:36 +0800

    add chm support.
    listchm:
        list html files in given file.chm
        e.g. ./listchm file.chm
    takechm:
        extract given html and related images from given chm file to given path, also cleans html format
        e.g. ./takechm file.chm /main.htm /mnt/ramdisk

---
+++ chm/listchm.c
@@ -0,0 +1,42 @@
+#include "chm_lib.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <strings.h>
+
+int print_html(struct chmFile *h,
+              struct chmUnitInfo *ui,
+              void *context)
+{
+    if(ui->flags & CHM_ENUMERATE_FILES) {
+        char *p = strrchr(ui->path, '.');
+        if (p && (strcasecmp(p, ".htm") == 0 || strcasecmp(p, ".html") == 0)) {
+            printf("%s\n", ui->path);
+        }
+    }
+	
+    return CHM_ENUMERATOR_CONTINUE;
+}
+
+int main(int argc, const char *argv[])
+{
+    struct chmFile *h;
+
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s chmfile\n", argv[0]);
+    }
+
+    h = chm_open(argv[1]);
+    if (h == NULL)
+    {
+        fprintf(stderr, "failed to open %s\n", argv[1]);
+        exit(1);
+    }
+
+    if (! chm_enumerate(h, CHM_ENUMERATE_ALL, print_html, NULL)) {
+        fprintf(stderr, "file format error\n"); exit(1); }
+
+    chm_close(h);
+
+    return 0;
+}
